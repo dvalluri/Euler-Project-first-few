ab43a898cc5d7651c3a03c5eb487445112801f38
dvalluri <dvalluri@uwo.ca>
2022-06-19 14:00:35 -0400

    Create ep15.py

---
+++ ep15.py
@@ -0,0 +1,13 @@
+def paths(L):
+    dict = {}
+    dict[(1,1)] = 2
+    for m in range(2,L+1):
+        dict[(m,1)] = dict[(m-1,1)] + 1
+        dict[(1,m)] = dict[(m,1)]
+    for n in range(2,L+1):
+        for m in range(2,L+1):
+             dict[(m,n)] = dict[(m-1,n)] + dict[(m,n-1)]
+
+    return dict[(L,L)]
+
+print(paths(20))
